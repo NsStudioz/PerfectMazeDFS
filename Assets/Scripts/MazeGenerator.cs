2f3bdfc5a5ef3eb60043f42bd7f1013dc049d4e4
NsStudioz <sneve2016@gmail.com>
2023-07-10 11:55:22 +0300

    Update: MazeGenerator.cs = New Vars.

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -6,7 +6,20 @@ namespace DFS_MazeGenerator
 {
     public class MazeGenerator : MonoBehaviour
     {
+        [Header("Maze Size")]
+        [SerializeField]
+        [Range(10, 250)]
+        private int mazeWidth;
 
-    }
+        [SerializeField]
+        [Range(10, 250)]
+        private int mazeHeight;
+
+        [Header("Cell Elements")]
+        [SerializeField] private Cell cellPrefab;
+        [SerializeField] private List<Cell> totalCells;
+        [SerializeField] private List<Cell> visitedCells;
+
+
 }
 
