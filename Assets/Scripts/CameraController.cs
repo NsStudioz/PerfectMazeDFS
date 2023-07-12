58002f3a24e4c8f560d8a94825e76aa86e12f7b9
NsStudioz <sneve2016@gmail.com>
2023-07-12 18:23:41 +0300

    Update: CameraController.cs = Position Calculation & Update.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private Vector3 newCameraPos = Vector3.zero;
+    [SerializeField] private float moveSpeed; 
 
     private int minValue = -250;
     private int maxValue = 250;
@@ -13,13 +14,62 @@ public class CameraController : MonoBehaviour
     private void Start()
     {
         ClampCameraPositions();
-        transform.position = newCameraPos;
+        UpdateNewPosition();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            MoveCameraUp();
+
+        else if (Input.GetKey(KeyCode.S))
+            MoveCameraDown();
+
+        if (Input.GetKey(KeyCode.D))
+            MoveCameraRight();
+
+        else if (Input.GetKey(KeyCode.A))
+            MoveCameraLeft();
     }
 
     private void ClampCameraPositions()
     {
         newCameraPos.x = Mathf.Clamp(minValue, 0, maxValue);
         newCameraPos.z = Mathf.Clamp(minValue, 0, maxValue);
+    }
+
+    private void MoveCameraUp()
+    {
+        newCameraPos.z += MovementSpeed();
+        UpdateNewPosition();
+    }
+
+    private void MoveCameraDown()
+    {
+        newCameraPos.z -= MovementSpeed();
+        UpdateNewPosition();
+    }
+
+    private void MoveCameraRight()
+    {
+        newCameraPos.x += MovementSpeed();
+        UpdateNewPosition();
+    }
+
+    private void MoveCameraLeft()
+    {
+        newCameraPos.x -= MovementSpeed();
+        UpdateNewPosition();
+    }
+
+    private float MovementSpeed()
+    {
+        return moveSpeed * Time.deltaTime;
+    }
+
+    private void UpdateNewPosition()
+    {
+        transform.position = newCameraPos;
     }
 
 }
