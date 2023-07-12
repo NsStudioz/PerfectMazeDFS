58b870a672d5bd59c929748d57c1a61f7685f648
NsStudioz <sneve2016@gmail.com>
2023-07-12 18:15:43 +0300

    Update: CameraController.cs = Vector3 clamping.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [SerializeField] private Vector3 newCameraPos = Vector3.zero;
+
+    private int minValue = -250;
+    private int maxValue = 250;
 
 
+    private void Start()
+    {
+        ClampCameraPositions();
+        transform.position = newCameraPos;
+    }
 
+    private void ClampCameraPositions()
+    {
+        newCameraPos.x = Mathf.Clamp(minValue, 0, maxValue);
+        newCameraPos.z = Mathf.Clamp(minValue, 0, maxValue);
+    }
 
 }
