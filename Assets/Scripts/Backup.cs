6eb7649679e6ce605f79a492c37bec9bdbd65c49
NsStudioz <sneve2016@gmail.com>
2023-07-12 12:05:14 +0300

    Ignore.

--- Assets/Scripts/Backup.cs
+++ Assets/Scripts/Backup.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace DFS_MazeGenerator
 {
@@ -262,6 +265,134 @@ namespace DFS_MazeGenerator
 
     }
 
+
+
+    public class UI_Backup
+    {
+        // Events:
+        public static event Action<int> OnClickWidthValueChange;
+        public static event Action<int> OnClickHeightValueChange;
+        public static event Action<bool> OnClickMazeGenerationToggleChange;
+
+
+        [Header("Panels")]
+        [SerializeField] private GameObject menuPanel;
+        [SerializeField] private GameObject gamePanel;
+
+        [Header("Maze Value Texts")]
+        [SerializeField] private TMP_Text widthValueText;
+        [SerializeField] private TMP_Text heightValueText;
+
+        [Header("Sliders")]
+        [SerializeField] private Slider widthSilder;
+        [SerializeField] private Slider heightSilder;
+
+        [Header("Buttons")]
+        [SerializeField] private Button generateMazeBtn;
+        [SerializeField] private Button playBtn;
+        [SerializeField] private Button backBtn;
+        [SerializeField] private Toggle generationToggle;
+
+        public static event Action OnClickGenerateMaze;
+        public static event Action OnClickDestroyMaze;
+        private bool isFastestGeneration = false;
+
+        private void Awake() => ConvertSlidersToInt();
+
+        private void ConvertSlidersToInt()
+        {
+            widthSilder.wholeNumbers = true;
+            heightSilder.wholeNumbers = true;
+        }
+
+        private void OnEnable()
+        {
+            // Buttons:
+            generateMazeBtn.onClick.AddListener(GenerateMaze);
+            playBtn.onClick.AddListener(StartMazeGame);
+            backBtn.onClick.AddListener(ReturnToMainMenu);
+            // Sliders:
+            widthSilder.onValueChanged.AddListener(AdjustMazeWidth);
+            heightSilder.onValueChanged.AddListener(AdjustMazeHeight);
+            // Toggle:
+            generationToggle.onValueChanged.AddListener(SetMazeGenerationMode);
+        }
+
+        private void OnDisable()
+        {
+            // Buttons:
+            generateMazeBtn.onClick.RemoveAllListeners();
+            playBtn.onClick.RemoveAllListeners();
+            backBtn.onClick.RemoveAllListeners();
+            // Sliders:
+            widthSilder.onValueChanged.RemoveAllListeners();
+            heightSilder.onValueChanged.RemoveAllListeners();
+            // Toggle:
+            generationToggle.onValueChanged.RemoveAllListeners();
+        }
+
+        void AdjustMazeWidth(float value)
+        {
+            SetMazeWidth(Mathf.RoundToInt(value));
+        }
+        void AdjustMazeHeight(float value)
+        {
+            SetMazeHeight(Mathf.RoundToInt(value));
+        }
+
+        private void SetMazeWidth(int widthValue)
+        {
+            widthSilder.value = widthValue;
+            AdjustWidthText(widthValue);
+            OnClickWidthValueChange?.Invoke(widthValue);
+        }
+        private void SetMazeHeight(int heightValue)
+        {
+            heightSilder.value = heightValue;
+            AdjustHeightText(heightValue);
+            OnClickHeightValueChange?.Invoke(heightValue);
+        }
+
+        private void AdjustWidthText(int widthValue)
+        {
+            widthValueText.text = widthValue.ToString();
+        }
+        private void AdjustHeightText(int heightValue)
+        {
+            heightValueText.text = heightValue.ToString();
+        }
+
+
+        private void GenerateMaze()
+        {
+            OnClickGenerateMaze?.Invoke();
+        }
+
+        private void StartMazeGame() => ShowGamePanel();
+
+        private void ReturnToMainMenu()
+        {
+            ShowMenuPanel();
+            OnClickDestroyMaze?.Invoke();
+        }
+
+        private void ShowGamePanel()
+        {
+            menuPanel.SetActive(false);
+            gamePanel.SetActive(true);
+        }
+        private void ShowMenuPanel()
+        {
+            menuPanel.SetActive(true);
+            gamePanel.SetActive(false);
+        }
+
+        private void SetMazeGenerationMode(bool state)
+        {
+            isFastestGeneration = !state;
+            OnClickMazeGenerationToggleChange?.Invoke(state);
+        }
+    }
 }
 
 
