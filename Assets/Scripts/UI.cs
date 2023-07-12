07435342302d06bac0029eccc98540d9d33608b6
NsStudioz <sneve2016@gmail.com>
2023-07-12 11:23:43 +0300

    New: UI.cs.

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using DFS_MazeGenerator;
+using UnityEditor.PackageManager;
+using UnityEngine.Events;
+
+public class UI : MonoBehaviour
+{
+
+
+
+
+
+}
