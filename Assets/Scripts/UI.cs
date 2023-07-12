db5585df0dc8e3f7039754d336c539cc2dbe8b91
NsStudioz <sneve2016@gmail.com>
2023-07-12 11:24:30 +0300

    Update: UI.cs = new vars and events for code to UI interactions

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -11,8 +11,32 @@ using UnityEngine.Events;
 public class UI : MonoBehaviour
 {
 
+    // Events:
+    public static event Action<int> OnClickWidthValueChange;
+    public static event Action<int> OnClickHeightValueChange;
+    public static event Action<bool> OnClickMazeGenerationToggleChange;
 
+    [Header("Panels")]
+    [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject gamePanel;
 
+    [Header("Maze Value Texts")]
+    [SerializeField] private TMP_Text widthValueText;
+    [SerializeField] private TMP_Text heightValueText;
+
+    [Header("Sliders")]
+    [SerializeField] private Slider widthSilder;
+    [SerializeField] private Slider heightSilder;
+
+    [Header("Buttons")]
+    [SerializeField] private Button generateMazeBtn;
+    [SerializeField] private Button playBtn;
+    [SerializeField] private Button backBtn;
+    [SerializeField] private Toggle generationToggle;
+
+    public static event Action OnClickGenerateMaze;
+    public static event Action OnClickDestroyMaze;
+    private bool isFastestGeneration = false;
 
 
 }
